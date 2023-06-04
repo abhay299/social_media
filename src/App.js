de9423973d0a20ab5cf3332e956a2d42b62ae715
Abhay Gupta <abhaysg2000@gmail.com>
2023-06-05 00:48:04 +0530

    Login and Register Pages authenticated

--- src/App.js
+++ src/App.js
@@ -1,19 +1,56 @@
 import './App.css';
-import Login from './pages/login/login';
-import Register from './pages/register/register';
-import { createBrowserRouter, RouterProvider, Route } from "react-router-dom";
+import LeftBar from './components/leftBar/LeftBar';
+import Navbar from './components/navbar/Navbar';
+import RightBar from './components/rightBar/RightBar';
+import Login from './pages/login/Login';
+import Register from './pages/register/Register';
+import Home from './pages/home/Home';
+import Profile from './pages/profile/Profile';
+
+import { createBrowserRouter, RouterProvider, Route, Outlet, Navigate } from "react-router-dom";
 
 function App() {
+  const currentUser = true;
 
   const Layout = () => {
     return (
       <div>
-
+        <Navbar />
+        <div style={{ display: "flex" }}>
+          <LeftBar />
+          <Outlet />
+          <RightBar />
+        </div>
       </div>
-    )
-  }
+    );
+  };
+
+  const ProtectedRoute = ({ children }) => {
+    if (!currentUser) {
+      return <Navigate to="/login" />
+    }
+
+    return children;
+  };
 
   const router = createBrowserRouter([
+    {
+      path: "/",
+      element:
+        <ProtectedRoute>
+          <Layout />
+        </ProtectedRoute>,
+      children: [
+        {
+          path: "/",
+          element: <Home />
+        },
+        {
+          path: "/profile/:id",
+          element: <Profile />
+        }
+      ]
+    },
     {
       path: "/login",
       element: <Login />,
