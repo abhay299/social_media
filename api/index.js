03a4f97c43d34220a3ae79593f7a17db7e7d49a0
Abhay Gupta <abhaysg2000@gmail.com>
2023-06-07 18:51:37 +0530

    server side initial commit

---
+++ api/index.js
@@ -0,0 +1,11 @@
+import Express from "express";
+const app = Express();
+import userRoutes from "./routes/users.js"
+
+
+
+app.use("/api/users", userRoutes);
+
+app.listen(8800, () => {
+	console.log("API working!");
+});
