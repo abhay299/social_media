a0b00c85fde42a999a52df98f6d189f7c65228c5
Abhay Gupta <abhaysg2000@gmail.com>
2023-06-08 03:20:49 +0530

    register/login authenticated

--- api/index.js
+++ api/index.js
@@ -1,10 +1,25 @@
-import Express from "express";
-const app = Express();
+import express from "express";
 import userRoutes from "./routes/users.js"
+import authRoutes from "./routes/auth.js"
+import postRoutes from "./routes/posts.js"
+import commentRoutes from "./routes/comments.js"
+import likeRoutes from "./routes/likes.js"
+import cors from "cors";
+import cookieParser from "cookie-parser";
 
 
+const app = express();
 
+//MIDDLEWARES
+app.use(express.json());
+app.use(cors());
+app.use(cookieParser());
+
+app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
+app.use("/api/posts", postRoutes);
+app.use("/api/comments", commentRoutes);
+app.use("/api/likes", likeRoutes);
 
 app.listen(8800, () => {
 	console.log("API working!");
