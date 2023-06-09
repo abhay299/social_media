cb4cee43d06ed3b08883caeccad7fcd2e97f1661
Abhay Gupta <abhaysg2000@gmail.com>
2023-06-10 03:33:32 +0530

    posts can be added now

--- api/controllers/post.js
+++ api/controllers/post.js
@@ -1,10 +1,48 @@
+import moment from "moment/moment.js";
 import { db } from "../connect.js";
+import jwt from "jsonwebtoken";
 
 export const getPosts = (req, res) => {
-	const q = `SELECT p.*, u.id AS userId, name, profilePic FROM posts AS p JOIN users AS u ON (u.id = p.userId)`
 
-	db.query(q, (err, data) => {
-		if (err) return res.status(200).json(err);
-		return res.status(200).json(data);
-	})
-}
+	const token = req.cookies.accessToken;
+	if (!token) return res.status(401).json("Not logged in!");
+
+	jwt.verify(token, "secretKey", (err, userInfo) => {
+		if (err) return res.status(403).json("Token is not valid");
+
+		const q = `SELECT p.*, u.id AS userId, name, profilePic FROM posts AS p JOIN users AS u ON (u.id = p.userId)
+		LEFT JOIN relationships AS r ON (p.userId = r.followedUserId) WHERE r.followerUserId = ? OR p.userId = ?
+		ORDER BY p.createdAt DESC`;
+
+		db.query(q, [userInfo.id, userInfo.id], (err, data) => {
+			if (err) return res.status(500).json(err);
+			return res.status(200).json(data);
+		});
+	});
+
+};
+
+export const addPost = (req, res) => {
+
+	const token = req.cookies.accessToken;
+	if (!token) return res.status(401).json("Not logged in!");
+
+	jwt.verify(token, "secretKey", (err, userInfo) => {
+		if (err) return res.status(403).json("Token is not valid");
+
+		const q = "INSERT INTO posts (`desc`, `img`, `createdAt`, `userId`) VALUES (?)";
+
+		const values = [
+			req.body.desc,
+			req.body.img,
+			moment(Date.now()).format("YYYY-MM-DD HH:mm:ss"),
+			userInfo.id
+		]
+
+		db.query(q, [userInfo.id, userInfo.id], (err, data) => {
+			if (err) return res.status(500).json(err);
+			return res.status(200).json(data);
+		});
+	});
+
+};
